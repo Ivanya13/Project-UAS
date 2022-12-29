5d3a42eeef290396d61bd7f9a97f9e1945157845
Lenovo <Lenovo@LAPTOP-CPOIOF56>
2022-12-29 15:09:28 +0700

    ganti text

---
+++ DailyClean/src/main/java/com/mycompany/dailyclean/DailyClean.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dailyclean;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DailyClean {
+
+    public static void main(String[] args) {
+        System.out.println("Hello you!");
+    }
+}
